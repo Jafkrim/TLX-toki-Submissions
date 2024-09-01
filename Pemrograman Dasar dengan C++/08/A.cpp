6341b4f2bb92604fc075766e37d5f1e43fb07bfd
Jaf_Alkarim <149105089+Jafkrim@users.noreply.github.com>
2024-09-01 20:20:55 +0700

    Create A.cpp
    As it said.

---
+++ Pemrograman Dasar dengan C++/08/A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long panjang = 2002419288;
+    int tinggi = 1040323716;
+
+    long long luas = panjang * tinggi;
+    cout << luas << endl;
+}
