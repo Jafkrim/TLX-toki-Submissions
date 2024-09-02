c18929c358083696f47cef1301bb335d8c4ff50d
Jaf_Alkarim <149105089+Jafkrim@users.noreply.github.com>
2024-09-02 12:42:54 +0700

    Create C.cpp
    As it said.

---
+++ Pemrograman Dasar dengan C++/09/C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void cetak_menurun(int n) {
+    if (n != 0){
+        cout << n << endl;
+        cetak_menurun(n - 1);
+    }
+}
+
+// Fungsi main() di bawah tidak boleh diubah!
+int main() {
+    cetak_menurun(10);
+}
